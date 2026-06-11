340bff219a43c83df4198692d56601b9a7f7def0
chrisghill <chris@massdriver.cloud>
2026-06-11 16:00:49 -0600

    remove top-level 'data' on all artifact-definitions

--- src/artifacts.tf
+++ src/artifacts.tf
@@ -1,8 +1,8 @@
 locals {
-  data_infrastructure = {
+  infrastructure = {
     ari = azurerm_cosmosdb_account.main.id
   }
-  data_authentication = {
+  authentication = {
     username = "${var.md_metadata.name_prefix}"
     password = azurerm_cosmosdb_account.main.primary_key
     hostname = "https://${var.md_metadata.name_prefix}.documents.cosmos.azure.com"
@@ -15,10 +15,8 @@ resource "massdriver_artifact" "authentication" {
   name     = "CosmosDB SQL Server ${var.md_metadata.name_prefix} (${azurerm_cosmosdb_account.main.id})"
   artifact = jsonencode(
     {
-      data = {
-        infrastructure = local.data_infrastructure
-        authentication = local.data_authentication
-      }
+      infrastructure = local.infrastructure
+      authentication = local.authentication
       specs = {
         azure = {
           region = azurerm_cosmosdb_account.main.location
